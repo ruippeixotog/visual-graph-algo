000c43126776da370464f734ef843a76ccd82767
Rui Gonçalves <ruippeixotog@gmail.com>
2013-12-20 00:07:25 +0000

    Improve graph model.

--- graph.js
+++ graph.js
@@ -1,45 +1,54 @@
 
 function newGraph(graphDef) {
   var graph = {
-    numNodes: graphDef[0][0],
-    numEdges: graphDef[0][1],
+    numNodes: 0,
+    numEdges: 0,
     adjs: [],
+    directed: false,
 
     addNode: function() {
-      graph.adjs.push([]);
-      return graph.numNodes++;
+      this.adjs.push([]);
+      return this.numNodes++;
     },
 
     removeNode: function(id) {
-      graph.adjs[id].forEach(function(adj) {
-        graph.adjs[adj].splice(graph.adjs[adj].indexOf(id), 1);
-      });
-      graph.adjs[id] = null;
+      if(this.directed) {
+        this.adjs[id].forEach(function(adj) {
+          this.adjs[adj].splice(this.adjs[adj].indexOf(id), 1);
+        });
+      }
+      this.adjs[id] = null;
+    },
+
+    hasEdge: function(src, dest) {
+      for(var i in this.adjs[src]) {
+        if(adjs[src][i] == dest) return true;
+      }
+      return false;
     },
 
     addEdge: function(src, dest) {
-      graph.adjs[src].push(dest);
-      graph.adjs[dest].push(src);
-      ++graph.numEdges;
+      this.adjs[src].push(dest);
+      if(!this.directed) this.adjs[dest].push(src);
+      this.numEdges++;
     },
 
     removeEdge: function(src, dest) {
-      graph.adjs[src].splice(graph.adjs[src].indexOf(dest), 1);
-      graph.adjs[dest].splice(graph.adjs[dest].indexOf(src), 1);
-      --graph.numEdges;
+      this.adjs[src].splice(this.adjs[src].indexOf(dest), 1);
+      if(!this.directed)
+        this.adjs[dest].splice(this.adjs[dest].indexOf(src), 1);
+      this.numEdges--;
     }
   };
 
   if(graphDef) {
-    for(var i = 0; i < graph.numNodes; i++)
-      graph.adjs[i] = [];
+    for(var i = 0; i < graphDef[0][0]; i++)
+      graph.addNode();
 
-    for(var i = 1; i <= graph.numEdges; i++) {
-      var from = graphDef[i][0] - 1;
-      var to = graphDef[i][1] - 1;
-
-      graph.adjs[from].push(to);
-      graph.adjs[to].push(from);
+    for(var i = 1; i <= graphDef[0][1]; i++) {
+      var src = graphDef[i][0] - 1;
+      var dest = graphDef[i][1] - 1;
+      graph.addEdge(src, dest);
     }
   }
   return graph;
