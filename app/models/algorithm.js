b70ad8dc90a9aba1dc451abc64db38453f5744b1
Rui Gonçalves <ruippeixotog@gmail.com>
2013-12-31 17:17:54 +0000

    Remove id property in algorithms (may be added later if needed).

--- app/models/algorithm.js
+++ app/models/algorithm.js
@@ -1,15 +1,9 @@
 VisualAlgo.Algorithm = Ember.Object.extend({
 
-  // --- to implement ---
-  id: function() {
-    return this.get("name");
-  }.property("name"),
-
   name: "",
   inputView: Ember.View.extend({}),
   startAlgo: function(graph) {},
-
-  // --- internals ---
+  
   init: function() {
     VisualAlgo.AlgorithmStore.get("algorithms").push(this);
   }
