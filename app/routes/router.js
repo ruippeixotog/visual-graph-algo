c8f8eff9a344f6dae57b1a8bee4b0fba511139eb
Rui Gonçalves <ruippeixotog@gmail.com>
2013-12-31 17:15:57 +0000

    Implement a proper algorithm route.

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,5 +1,6 @@
 require('app/routes/application_route');
+require('app/routes/algorithm_route');
 
 VisualAlgo.Router.map(function () {
-  this.resource('algorithm', { path: '/:algorithm_id' });
+  this.resource('algorithm', { path: '/:name' });
 });
