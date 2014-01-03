95d2a8ab82ee03522e58e3707144ccfe1ab629a5
Rui Gonçalves <ruippeixotog@gmail.com>
2014-01-03 01:00:09 +0000

    Remove unsupported "view" control event.

--- app/old/algoctl.js
+++ app/old/algoctl.js
@@ -1,8 +1,4 @@
 
 ctl = {
-  step: function() { return { step: true }; },
-
-  view: function(func) {
-    return { step: false, view: func };
-  }
+  step: function() { return { step: true }; }
 };
