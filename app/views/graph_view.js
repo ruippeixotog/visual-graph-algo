3fe94b0e06c0e42d0160791420e6834bdb7216f4
Rui Gonçalves <ruippeixotog@gmail.com>
2014-01-05 14:53:02 +0000

    Split restart function into atomic update operations and make them observers.

--- app/views/graph_view.js
+++ app/views/graph_view.js
@@ -10,14 +10,8 @@ VisualAlgo.GraphView = Ember.ContainerView.extend({
   // -------
   // graph data stored in a D3 handy way
 
-  d3nodes: function() {
-    return Ember.A([]);
-  }.property(), // TODO: to depend on a property of graph once it exists
-
-  d3links: function() {
-    return Ember.A([]);
-  }.property(), // TODO: to depend on a property of graph once it exists
-
+  d3nodes: [], // TODO: to depend on an observable property of graph once it exists
+  d3links: [], // TODO: to depend on an observable property of graph once it exists
   nextNodeId: 0,
 
   syncWithModel: function() {
@@ -36,7 +30,7 @@ VisualAlgo.GraphView = Ember.ContainerView.extend({
 
       nodeData.id = id;
       nodeData.view = nodeData.view || {};
-      d3nodes.push(nodeData);
+      d3nodes.pushObject(nodeData);
 
       graph.foreachAdj(id, function(adj, edgeData) {
         if(!graph.isDirected() && id < adj) return;
@@ -44,19 +38,32 @@ VisualAlgo.GraphView = Ember.ContainerView.extend({
         edgeData.source = nodeData;
         edgeData.target = graph.getNode(adj);
         edgeData.view = edgeData.view || {};
-        d3links.push(edgeData);
+        d3links.pushObject(edgeData);
       });
     });
   },
 
   // -------
-  // D3 state
+  // D3 mouse and selection state
 
   selectedNode: null,
   selectedLink: null,
   mousedownLink: null,
   mousedownNode: null,
   mouseupNode: null,
+
+  dragLine: function() {
+    return this.get('svg')
+      .append('svg:path')
+      .attr('class', 'link dragline hidden')
+      .attr('d', 'M0,0L0,0');
+  }.property('svg'),
+
+  resetMouseVars: function() {
+    this.set('mousedownNode', null);
+    this.set('mouseupNode', null);
+    this.set('mousedownLink', null);
+  },
 
   // -------
   // D3 view components
@@ -101,10 +108,6 @@ VisualAlgo.GraphView = Ember.ContainerView.extend({
 
   }.property('graph', 'svgWidth', 'svgHeight'),
 
-  onSvgChange: function() {
-    this.createView();
-  }.observes('svg'),
-
   forceLayout: function() {
     return d3.layout.force()
       .nodes(this.get('d3nodes'))
@@ -114,13 +117,176 @@ VisualAlgo.GraphView = Ember.ContainerView.extend({
       .charge(-500);
   }.property('d3nodes', 'd3links', 'svgWidth', 'svgHeight'),
 
-  path: function() {
+  // -------
+  // D3 node drawing
+
+  svgBaseNodes: function() {
+    return this.get('svg').append('svg:g').selectAll('g');
+  }.property('svg'),
+
+  svgNodes: function() {
+    return this.get('svgBaseNodes');
+  }.property(),
+
+  updateNodesInSvg: function() {
+    var view = this;
+    var svgNodes = view.get('svgNodes');
+
+    // update existing nodes (selected visual states)
+    svgNodes.selectAll('circle')
+      .attr('class', function(d) {
+        d.view = d.view || {};
+        return $.merge(['node'], d.view.classes || []).join(' ');
+      })
+      .classed('selected', function(d) { return d === view.get('selectedNode'); });
+
+  }.observes('selectedNode'),
+
+  addRemoveNodesInSvg: function() {
+    var view = this;
+    // NB: the function arg is crucial here! nodes are known by id, not by index!
+    var svgNodes = view.get('svgNodes').data(view.get('d3nodes'), function(d) { return d.id; });
+    this.set('svgNodes', svgNodes);
+
+    // add new nodes
+    var g = svgNodes.enter().append('svg:g');
+
+    g.append('svg:circle')
+      .attr('class', function(d) {
+        d.view = d.view || {};
+        return $.merge(['node'], d.view.classes || []).join(' ');
+      })
+      .attr('r', 12)
+      .classed('selected', function(d) { return d === view.get('selectedNode'); })
+      .attr('data-nodeid', function(d) { return d.id; })
+      .on('mouseover', function(d) {
+        if(!view.get('mousedownNode') || d === view.get('mousedownNode')) return;
+        // enlarge target node
+        d3.select(this).attr('transform', 'scale(1.1)');
+      })
+      .on('mouseout', function(d) {
+        if(!view.get('mousedownNode') || d === view.get('mousedownNode')) return;
+        // unenlarge target node
+        d3.select(this).attr('transform', '');
+      })
+      .on('mousedown', function(d) {
+        if(d3.event.ctrlKey) return;
+
+        // select node
+        view.set('mousedownNode', d);
+        if(view.get('mousedownNode') === view.get('selectedNode')) view.set('selectedNode', null);
+        else view.set('selectedNode', view.get('mousedownNode'));
+        view.set('selectedLink', null);
+
+        // reposition drag line
+        view.get('dragLine')
+          .style('marker-end', view.get('graph').isDirected() ? 'url(#end-arrow)' : '')
+          .classed('hidden', false)
+          .attr('d', 'M' + view.get('mousedownNode').x + ',' + view.get('mousedownNode').y +
+            'L' + view.get('mousedownNode').x + ',' + view.get('mousedownNode').y);
+      })
+      .on('mouseup', function(d) {
+        if(!view.get('mousedownNode')) return;
+
+        // needed by FF
+        view.get('dragLine')
+          .classed('hidden', true)
+          .style('marker-end', '');
+
+        // check for drag-to-self
+        view.set('mouseupNode', d);
+        if(view.get('mouseupNode') === view.get('mousedownNode')) { view.resetMouseVars(); return; }
+
+        // unenlarge target node
+        d3.select(this).attr('transform', '');
+
+        // add link to graph (update if exists)
+        var source = view.get('mousedownNode'),
+          target = view.get('mouseupNode');
+
+        var link = null;
+        if(!view.get('graph').getEdge(source.id, target.id)) {
+          link = view.get('graph').addEdge(source.id, target.id,
+            { source: source, target: target });
+          view.get('d3links').pushObject(link);
+        }
+
+        // select new link
+        view.set('selectedLink', link);
+        view.set('selectedNode', null);
+      });
+
+    // show node IDs
+    g.append('svg:text')
+      .attr('x', 0)
+      .attr('y', 4)
+      .attr('class', 'id')
+      .text(function(d) { return d.id; });
+
+    // remove old nodes
+    svgNodes.exit().remove();
+
+    // set the graph in motion
+    this.get('forceLayout').start();
+
+  }.observes('d3nodes.@each'),
+
+  // -------
+  // D3 link drawing
+
+  svgBaseLinks: function() {
     return this.get('svg').append('svg:g').selectAll('path');
   }.property('svg'),
 
-  circle: function() {
-    return this.get('svg').append('svg:g').selectAll('g');
-  }.property('svg'),
+  svgLinks: function() {
+    return this.get('svgBaseLinks');
+  }.property(),
+
+  updateLinksInSvg: function() {
+    var view = this;
+    var svgLinks = view.get('svgLinks');
+
+    // update existing links
+    svgLinks
+      .attr('class', function(d) { return $.merge(['link'], d.view.classes || []).join(' '); })
+      .classed('selected', function(d) { return d === view.get('selectedLink'); })
+      .style('marker-end', view.get('graph').isDirected() ? 'url(#end-arrow)' : '');
+
+  }.observes('selectedLink'),
+
+  addRemoveLinksInSvg: function() {
+    var view = this;
+    var svgLinks = view.get('svgLinks').data(view.get('d3links'));
+    this.set('svgLinks', svgLinks);
+
+    // add new links
+    svgLinks.enter().append('svg:path')
+      .attr('class', function(d) {
+        d.view = d.view || {};
+        return $.merge(['link'], d.view.classes || []).join(' ');
+      })
+      .classed('selected', function(d) { return d === view.get('selectedLink'); })
+      .style('marker-end', view.get('graph').isDirected() ? 'url(#end-arrow)' : '')
+      .on('mousedown', function(d) {
+        if(d3.event.ctrlKey) return;
+
+        // select link
+        view.set('mousedownLink', d);
+        if(view.get('mousedownLink') === view.get('selectedLink')) view.set('selectedLink', null);
+        else view.set('selectedLink', view.get('mousedownLink'));
+        view.set('selectedNode', null);
+      });
+
+    // remove old links
+    svgLinks.exit().remove();
+
+    // set the graph in motion
+    view.get('forceLayout').start();
+
+  }.observes('d3links.@each'),
+
+  // -------
+  // D3 initialization
 
   createView: function() {
     var view = this;
@@ -133,25 +299,9 @@ VisualAlgo.GraphView = Ember.ContainerView.extend({
     var force = view.get('forceLayout')
       .on('tick', tick);
 
-    // line displayed when dragging new nodes
-    var drag_line = svg.append('svg:path')
-      .attr('class', 'link dragline hidden')
-      .attr('d', 'M0,0L0,0');
-
-    // handles to link and node element groups
-    var path = this.get('path'),
-        circle = this.get('circle');
-
-    function resetMouseVars() {
-      view.set('mousedownNode', null);
-      view.set('mouseupNode', null);
-      view.set('mousedownLink', null);
-    }
-
-    // update force layout (called automatically each iteration)
     function tick() {
       // draw directed edges with proper padding from node centers
-      path.attr('d', function(d) {
+      view.get('svgLinks').attr('d', function(d) {
         var deltaX = d.target.x - d.source.x,
             deltaY = d.target.y - d.source.y,
             dist = Math.sqrt(deltaX * deltaX + deltaY * deltaY),
@@ -166,145 +316,9 @@ VisualAlgo.GraphView = Ember.ContainerView.extend({
         return 'M' + sourceX + ',' + sourceY + 'L' + targetX + ',' + targetY;
       });
 
-      circle.attr('transform', function(d) {
+      view.get('svgNodes').attr('transform', function(d) {
         return 'translate(' + d.x + ',' + d.y + ')';
       });
-    }
-
-    // update graph (called when needed)
-    function updateViewLinks() {
-      // path (link) group
-      path = path.data(view.get('d3links'));
-
-      // update existing links
-      path
-        .attr('class', function(d) { return $.merge(['link'], d.view.classes || []).join(' '); })
-        .classed('selected', function(d) { return d === view.get('selectedLink'); })
-        .style('marker-end', view.get('graph').isDirected() ? 'url(#end-arrow)' : '');
-
-
-      // add new links
-      path.enter().append('svg:path')
-        .attr('class', function(d) {
-          d.view = d.view || {};
-          return $.merge(['link'], d.view.classes || []).join(' ');
-        })
-        .classed('selected', function(d) { return d === view.get('selectedLink'); })
-        .style('marker-end', view.get('graph').isDirected() ? 'url(#end-arrow)' : '')
-        .on('mousedown', function(d) {
-          if(d3.event.ctrlKey) return;
-
-          // select link
-          view.set('mousedownLink', d);
-          if(view.get('mousedownLink') === view.get('selectedLink')) this.set('selectedLink', null);
-          else view.set('selectedLink', view.get('mousedownLink'));
-          view.set('selectedNode', null);
-          updateViewLinks();
-          updateViewNodes();
-        });
-
-      // remove old links
-      path.exit().remove();
-
-      // set the graph in motion
-      force.start();
-    }
-
-    // update graph (called when needed)
-    function updateViewNodes() {
-
-      // circle (node) group
-      // NB: the function arg is crucial here! nodes are known by id, not by index!
-      circle = circle.data(view.get('d3nodes'), function(d) { return d.id; });
-
-      // update existing nodes (selected visual states)
-      circle.selectAll('circle')
-        .attr('class', function(d) { return $.merge(['node'], d.view.classes || []).join(' '); })
-        .classed('selected', function(d) { return d === view.get('selectedNode'); });
-
-      // add new nodes
-      var g = circle.enter().append('svg:g');
-
-      g.append('svg:circle')
-        .attr('class', function(d) { d.view = {}; return $.merge(['node'], d.view.classes || []).join(' '); })
-        .attr('r', 12)
-        .classed('selected', function(d) { return d === view.get('selectedNode'); })
-        .attr('data-nodeid', function(d) { return d.id; })
-        .on('mouseover', function(d) {
-          if(!view.get('mousedownNode') || d === view.get('mousedownNode')) return;
-          // enlarge target node
-          d3.select(this).attr('transform', 'scale(1.1)');
-        })
-        .on('mouseout', function(d) {
-          if(!view.get('mousedownNode') || d === view.get('mousedownNode')) return;
-          // unenlarge target node
-          d3.select(this).attr('transform', '');
-        })
-        .on('mousedown', function(d) {
-          if(d3.event.ctrlKey) return;
-
-          // select node
-          view.set('mousedownNode', d);
-          if(view.get('mousedownNode') === view.get('selectedNode')) view.set('selectedNode', null);
-          else view.set('selectedNode', view.get('mousedownNode'));
-          view.set('selectedLink', null);
-
-          // reposition drag line
-          drag_line
-            .style('marker-end', view.get('graph').isDirected() ? 'url(#end-arrow)' : '')
-            .classed('hidden', false)
-            .attr('d', 'M' + view.get('mousedownNode').x + ',' + view.get('mousedownNode').y +
-              'L' + view.get('mousedownNode').x + ',' + view.get('mousedownNode').y);
-
-          updateViewLinks();
-          updateViewNodes();
-        })
-        .on('mouseup', function(d) {
-          if(!view.get('mousedownNode')) return;
-
-          // needed by FF
-          drag_line
-            .classed('hidden', true)
-            .style('marker-end', '');
-
-          // check for drag-to-self
-          view.set('mouseupNode', d);
-          if(view.get('mouseupNode') === view.get('mousedownNode')) { resetMouseVars(); return; }
-
-          // unenlarge target node
-          d3.select(this).attr('transform', '');
-
-          // add link to graph (update if exists)
-          // NB: links are strictly source < target; arrows separately specified by booleans
-          var source = view.get('mousedownNode'),
-            target = view.get('mouseupNode');
-
-          var link = null;
-          if(!view.get('graph').getEdge(source.id, target.id)) {
-            link = view.get('graph').addEdge(source.id, target.id,
-              { source: source, target: target });
-            view.get('d3links').push(link);
-          }
-
-          // select new link
-          view.set('selectedLink', link);
-          view.set('selectedNode', null);
-          updateViewLinks();
-          updateViewNodes();
-        });
-
-      // show node IDs
-      g.append('svg:text')
-        .attr('x', 0)
-        .attr('y', 4)
-        .attr('class', 'id')
-        .text(function(d) { return d.id; });
-
-      // remove old nodes
-      circle.exit().remove();
-
-      // set the graph in motion
-      force.start();
     }
 
     function mousedown() {
@@ -320,26 +334,24 @@ VisualAlgo.GraphView = Ember.ContainerView.extend({
       var point = d3.mouse(this),
           node = view.get('graph').addNode(view.get('nextNodeId'),
             { id: view.get('nextNodeId'), x: point[0], y: point[1] });
-      view.get('d3nodes').push(node);
+      view.get('d3nodes').pushObject(node);
 
       view.set('nextNodeId', view.get('nextNodeId') + 1);
-      updateViewNodes();
     }
 
     function mousemove() {
       if(!view.get('mousedownNode')) return;
 
       // update drag line
-      drag_line.attr('d', 'M' + view.get('mousedownNode').x + ',' + view.get('mousedownNode').y +
+      view.get('dragLine')
+        .attr('d', 'M' + view.get('mousedownNode').x + ',' + view.get('mousedownNode').y +
         'L' + d3.mouse(this)[0] + ',' + d3.mouse(this)[1]);
-
-      updateViewLinks();
     }
 
     function mouseup() {
       if(view.get('mousedownNode')) {
         // hide drag line
-        drag_line
+        view.get('dragLine')
           .classed('hidden', true)
           .style('marker-end', '');
       }
@@ -348,15 +360,15 @@ VisualAlgo.GraphView = Ember.ContainerView.extend({
       svg.classed('active', false);
 
       // clear mouse event vars
-      resetMouseVars();
+      view.resetMouseVars();
     }
 
-    function spliceLinksForNode(node) {
+    function removeLinksForNode(node) {
       var toSplice = view.get('d3links').filter(function(l) {
         return (l.source === node || l.target === node);
       });
       toSplice.map(function(l) {
-        view.get('d3links').splice(view.get('d3links').indexOf(l), 1);
+        view.get('d3links').replace(view.get('d3links').indexOf(l), 1);
       });
     }
 
@@ -371,7 +383,7 @@ VisualAlgo.GraphView = Ember.ContainerView.extend({
 
       // ctrl
       if(d3.event.keyCode === 17) {
-        circle.call(force.drag);
+        view.get('svgNodes').call(force.drag);
         svg.classed('ctrl', true);
       }
 
@@ -382,22 +394,19 @@ VisualAlgo.GraphView = Ember.ContainerView.extend({
 
         case 46: // delete
           if(view.get('selectedNode')) {
-            view.get('d3nodes').splice(view.get('d3nodes').indexOf(view.get('selectedNode')), 1);
-            spliceLinksForNode(view.get('selectedNode'));
+            view.get('d3nodes').replace(view.get('d3nodes').indexOf(view.get('selectedNode')), 1);
+            removeLinksForNode(view.get('selectedNode'));
             view.get('graph').removeNode(view.get('selectedNode').id);
 
             view.set('selectedNode', null);
-            updateViewLinks();
-            updateViewNodes();
 
           } else if(view.get('selectedLink')) {
-            view.get('d3links').splice(view.get('d3links').indexOf(view.get('selectedLink')), 1);
+            view.get('d3links').replace(view.get('d3links').indexOf(view.get('selectedLink')), 1);
             view.get('graph').removeEdge(
               view.get('selectedLink').source.id,
               view.get('selectedLink').target.id);
 
             view.set('selectedLink', null);
-            updateViewLinks();
           }
           break;
       }
@@ -408,7 +417,7 @@ VisualAlgo.GraphView = Ember.ContainerView.extend({
 
       // ctrl
       if(d3.event.keyCode === 17) {
-        circle
+        view.get('svgNodes')
           .on('mousedown.drag', null)
           .on('touchstart.drag', null);
         svg.classed('ctrl', false);
@@ -422,22 +431,24 @@ VisualAlgo.GraphView = Ember.ContainerView.extend({
     d3.select(window)
       .on('keydown', keydown)
       .on('keyup', keyup);
-    updateViewLinks();
-    updateViewNodes();
-
-    // hyper mega hack to expose the restart functions
-    this.updateViewLinks = updateViewLinks;
-    this.updateViewNodes = updateViewNodes;
   },
 
   redraw: function() {
     this.syncWithModel();
-    this.updateViewLinks();
-    this.updateViewNodes();
+    this.addRemoveNodesInSvg();
+    this.addRemoveLinksInSvg();
+    this.updateNodesInSvg();
+    this.updateLinksInSvg();
 
     if(this.get('shouldRedraw'))
       this.set('shouldRedraw', false);
   }.observes('shouldRedraw', 'graph.directed'),
+
+  onSvgChange: function() {
+    this.set('svgNodes', this.get('svgBaseNodes'));
+    this.set('svgLinks', this.get('svgBaseLinks'));
+    this.createView();
+  }.observes('svg'),
 
   // -------
   // Ember related initialization
